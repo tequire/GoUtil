b89095b41ce21a7a522df2aa4fb89928d57a8a0d
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-06-28 22:38:03 +0200

    Updated config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,3 +5,9 @@ const DevIdentityServer = "https://identity-dev.highered.global"
 
 // ProdIdentityServer is the URL for the dev identity server
 const ProdIdentityServer = "https://identity.highered.global"
+
+// DevJobAdsAPI is the URL for the dev JobAdsAPI
+const DevJobAdsAPI = "https://highered-identity-jobad-dev.azurewebsites.net"
+
+// ProdJobAdsAPI is the URL for the prod JobAdsAPI
+const ProdJobAdsAPI = "https://highered-jobadsapi-identity-prod.azurewebsites.net"
