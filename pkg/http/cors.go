a321fe8cf9a13dccbcd67bf68ce3ec4527b5811f
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-07-08 23:07:44 +0200

    Added extra headers to cors-method

--- pkg/http/cors.go
+++ pkg/http/cors.go
@@ -2,11 +2,13 @@ package http
 
 import "net/http"
 
+// Cors sets cors headers, and returns true if request is an OPTIONS request
 func Cors(w http.ResponseWriter, r *http.Request) bool {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	if r.Method == "OPTIONS" {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With")
 		return true
 	}
 	return false
